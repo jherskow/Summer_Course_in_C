c8a386bb260e7036b937799524cdc3e276a3bbaa
Josh Herskowitz <jjherskow@gmail.com>
2017-08-01 12:07:32 +0300

    c

---
+++ CodingStyleExample.c
@@ -0,0 +1,81 @@
+/**
+ * @file ex1.c
+ * @author  Jherskow
+ *
+ * @brief
+ *
+ * @section DESCRIPTION
+ * The system keeps track of the cooking times.
+ * Input  :
+ * Process:
+ * Output :
+ */
+
+// ------------------------------ includes ------------------------------
+#include <stdio.h>
+
+
+// -------------------------- const definitions -------------------------
+/**
+ * @def MAX_TEMPERATURE_CS 300
+ * @brief A macro that sets the maximal temperature.
+ */
+#define MAX_TEMPERATURE_CS 300
+
+/**
+ * @var int NUM_HEAT_LEVELS
+ * @brief Sets the number of heat levels.
+ */
+const int NUM_HEAT_LEVELS = 5;
+
+// ------------------------------ functions -----------------------------
+/**
+ * @brief The main function. Actually, does nothing here.
+ * @return 0, to tell the system the execution ended without errors.
+ */
+int main()
+{
+    return 0;
+}
+
+/**
+ * @brief Perform time measurement.
+ */
+void performMeasurement()
+{
+    // Variable definition
+
+    // Code section
+}
+
+/**
+ * @brief Calculate sum of times and return it.
+ * @param time1 First time
+ * @param time2 Second time
+ * @return Addition of the two times
+ */
+int addTwoTimes(const int time1, const int time2)
+{
+    // Local variables:
+    double localVariable1; // In real programs there should be
+    // a meaningful name for the variable.
+
+    return (time1 + time2);
+}
+
+/**
+ * @brief Just a long parameter list example.
+ * @param a First arg.
+ * @param b Second arg.
+ * ...
+ * @param j Last arg.
+ */
+void functionA(int a, int b, int c, int d, int e, double f, double g,
+               double h, double i, double j)
+{
+    int aA = (2 + 3); // OK. It's ok not to provide spaces around [, (, {
+    int bB = -2; // OK. Minus can be used to indicate minus value.
+    for (a; b; c); // OK
+    hello(a, b, c); // OK
+    tt[c++]; // OK. Unary operator is used in the [ ( [
+}
