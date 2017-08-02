29944057028147ccfe8349e1730fdb8a722bb38b
jherskow <jjherskow@gmail.com>
2017-08-02 11:12:12 +0300

    more pushing for formula

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -46,7 +46,7 @@ long double calculateD1(long double x, long double x)
  * @brief
  * @return
  */
-long double calculateD2(long double x, long double x)
+long double calculateD2(long double x, long double* y)
 {
     long double term = powl((x+ALPHA), 2) + powl(y,2);
     return powl(term, 1.5);
@@ -56,7 +56,7 @@ long double calculateD2(long double x, long double x)
  * @brief
  * @return
  */
-long double calculateAx(long double x, long double vY, long double d1, long double d2)
+long double calculateAlphaX(long double x, long double vY, long double d1, long double d2)
 {
     return x + 2*vY - BETA*((x+ALPHA)/d1) - ALPHA*((x-BETA)/d2);
 }
@@ -65,7 +65,7 @@ long double calculateAx(long double x, long double vY, long double d1, long doub
  * @brief
  * @return
  */
-long double calculateAy(long double y, long double vX, long double d1, long double d2)
+long double calculateAlphaY(long double y, long double vX, long double d1, long double d2)
 {
     return y + 2*vX - BETA*(y/d1) - ALPHA*(y/d2);
 }
@@ -80,8 +80,14 @@ long double calculateInitialVelocity(long double x, long double y,  long double 
 long double calculateSingleStep(long double* x, long double* y,  long double* velX,  long double* velY,
                                 long double differenceTime)
 {
-    forwardZ(x, velX, alphaX, &differenceTime );
-    forwardZ(y, velY, alphaY, &differenceTime );
+    long double d1 = calculateD1(x, y);
+    long double d2 = calculateD2(x, y);
+
+    long double alphaX = calculateAlphaX(x, velX, d1, d2);
+    long double alphaY = calculateAlphaY(x, velY, d1, d2);
+
+    forwardZ(x, velX, alphaX, differenceTime );
+    forwardZ(y, velY, alphaY, differenceTime );
 }
 
 long double forwardZ(long double* z, long double* velZ, long double alphaZ, long double differenceTime)
