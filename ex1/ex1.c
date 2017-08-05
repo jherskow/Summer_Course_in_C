414872d484e211f9e996d42f1e3a1a46316aaccb
jherskow <jjherskow@gmail.com>
2017-08-05 08:53:47 +0300

    made use of global variables to simplify code

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -32,6 +32,8 @@ const long double ALPHA =  0.012299;
  */
 const long double BETA =  1 - 0.012299;
 
+const long double ZERO =  0;
+
 const int IO_ERROR =  2;
 const int NM_ERROR =  3;
 const int TIME_ERROR =  4;
@@ -39,7 +41,7 @@ const int TIME_ERROR =  4;
 
 // -------------------------- global definitions ------------------------
 
-long double *x, *y , *velX, *velY, time;
+long double x, y , velX, velY, time, differenceTime;
 int n, m;
 
 // ------------------------------ functions -----------------------------
@@ -47,7 +49,7 @@ int n, m;
  * @brief
  * @return
  */
-long double calculateD1(long double x, long double y)
+long double calculateD1()
 {
     long double term = powl((x-BETA), 2) + powl(y,2);
     return powl(term, 1.5);
@@ -57,7 +59,7 @@ long double calculateD1(long double x, long double y)
  * @brief
  * @return
  */
-long double calculateD2(long double x, long double y)
+long double calculateD2()
 {
     long double term = powl((x+ALPHA), 2) + powl(y,2);
     return powl(term, 1.5);
@@ -67,18 +69,18 @@ long double calculateD2(long double x, long double y)
  * @brief
  * @return
  */
-long double calculateAx(long double x, long double vY, long double d1, long double d2)
+long double calculateAx(long double d1, long double d2)
 {
-    return x + 2*vY - BETA*((x+ALPHA)/d1) - ALPHA*((x-BETA)/d2);
+    return x + 2*velY - BETA*((x+ALPHA)/d1) - ALPHA*((x-BETA)/d2);
 }
 
 /**
  * @brief
  * @return
  */
-long double calculateAy(long double y, long double vX, long double d1, long double d2)
+long double calculateAy(long double d1, long double d2)
 {
-    return y - 2*vX - BETA*(y/d1) - ALPHA*(y/d2);
+    return y - 2*velX - BETA*(y/d1) - ALPHA*(y/d2);
 }
 
 //long double calculateInitialVelocity(long double x, long double y,  long double vX,  long double vY,  long double time,
@@ -92,7 +94,7 @@ long double calculateAy(long double y, long double vX, long double d1, long doub
  * @brief
  * @return
  */
-long double forwardZ(long double* z, long double* velZ, long double alphaZ, long double differenceTime)
+long double forwardZ(long double* z, long double* velZ, long double alphaZ)
 {
     *z = (*z) + (*velZ)*(differenceTime);
     *velZ = (*velZ) + (alphaZ)*(differenceTime);
@@ -102,33 +104,30 @@ long double forwardZ(long double* z, long double* velZ, long double alphaZ, long
 /**
  * @brief
  */
-void eulerSingleStep(long double* x, long double* y,  long double* velX,  long double* velY,
-                                long double differenceTime)
+void eulerSingleStep()
 {
-    long double d1 = calculateD1(*x, *y);
-    long double d2 = calculateD2(*x, *y);
+    long double d1 = calculateD1();
+    long double d2 = calculateD2();
 
-    long double alphaX = calculateAx(*x, *velY, d1, d2);
-    long double alphaY = calculateAy(*x, *velY, d1, d2);
+    long double alphaX = calculateAx(d1, d2);
+    long double alphaY = calculateAy(d1, d2);
 
-    forwardZ(x, velX, alphaX, differenceTime );
-    forwardZ(y, velY, alphaY, differenceTime );
+    forwardZ(&x, &velX, alphaX);
+    forwardZ(&y, &velY, alphaY);
 }
 /**
  * @brief
  */
-void calculatePath(long double* x, long double* y,  long double* velX,  long double* velY,
-                         long double time, int n, int m)
+void calculatePath()
 {
-
     long double differenceTime = time/n;
     for (int i = 1; i <= n; i++)
     {
-        eulerSingleStep(x, y, velX, velY, differenceTime);
-        if(i%m) //todo fix - needs to print precisely m times
+        eulerSingleStep();
+        if(i%(n/m))
         {
-            //todo
-            printf("<%Lf>,<%Lf>,", *x, *y);
+            //todo correct format?
+            printf("<%Lf>,<%Lf>,", x, y);
         }
     }
 }
@@ -138,7 +137,7 @@ long double getLongDouble()
     // todo get long double
     int length = sizeof(long double)/sizeof(char);
 
-    long double x = sscanf(,)
+    // todo long double x = sscanf(,)
 
     // if error
     sprintf(stderr,"Input Error:\nInvalid input for _long double_");
@@ -149,7 +148,7 @@ int getInt()
 {
     // todo get int
 
-    long double x = str;
+   //todo long double x = str;
 
     // if error
     sprintf(stderr,"Input Error:\nInvalid input for _int_");
@@ -159,14 +158,14 @@ int getInt()
 int getInputFromUser()
 {
     printf("Enter initial pos x:\n");
-    *x = getLongDouble();
+    x = getLongDouble();
     printf("Enter initial pos y:\n");
-    *y = getLongDouble();
+    y = getLongDouble();
 
     printf("Enter initial vel x:\n");
-    *velX = getLongDouble();
+    velX = getLongDouble();
     printf("Enter initial vel y:\n");
-    *velY = getLongDouble();
+    velY = getLongDouble();
 
     printf("Enter num of steps to save:\n");
     n = getInt();
